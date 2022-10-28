7be7c62759012c05e39c0dd095fd5f6bf991cfa3
JONATHAN LEE <jkl45@sfu.ca>
2022-10-27 20:35:40 -0700

    added a city script java

---
+++ js/cityScript.js
@@ -0,0 +1,10 @@
+<script>
+function openStory(storyName){
+  var i;
+  var x = document.getElementsByClassName("story");
+  for(i=0;i<x.length;i++){
+    x[i].style.display="none";
+  }
+  document.getElementById(storyName).style.display="block";
+}
+</script>
